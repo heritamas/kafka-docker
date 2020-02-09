c6e69bdbc23100d1785dbf938274222f0097025b
heritamas <htamas@cloudera.com>
2020-02-09 01:07:57 +0100

    specified volumes for persistent storage

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+docker run \
+  --rm \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --network kafkadocker_default \
+  -i \
+  -t \
+  -e PS1="\u@\h:\w $ " \
+  wurstmeister/kafka \
+  /bin/bash
